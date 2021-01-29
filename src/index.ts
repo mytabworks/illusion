b940a2d698bbf7df0c4c382377177bcb056226bc
mytabworks <mytabworks@gmail.com>
2021-01-29 19:03:07 +0800

    🐞 fixed bug for classet alone illusion

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,42 @@
 interface IllussionStaticProps {
-    construct: (target: any, args: any) => any
-    get?: (target: any, p: string | number | symbol, receiver: any) => any
-    set?: (target: any, p: string | number | symbol, receiver: any) => any
+  construct: (target: any, args: any) => any;
+  get?: (target: any, p: string | number | symbol, receiver: any) => any;
+  set?: (target: any, p: string | number | symbol, receiver: any) => any;
 }
 
-interface IllussionInstanceProps extends Omit<IllussionStaticProps, 'construct'> {
-    has?: (target: any, p: string | number | symbol) => boolean
-    deleteProperty?: (target: any, p: string | number | symbol) => boolean
+interface IllussionInstanceProps
+  extends Omit<IllussionStaticProps, "construct"> {
+  has?: (target: any, p: string | number | symbol) => boolean;
+  deleteProperty?: (target: any, p: string | number | symbol) => boolean;
 }
 
-const __illusion__ = <I>($class: any, consider = false): I => {
+const __illusion__ = <I>($class: any): I => {
+  const prototype = $class.prototype;
 
-	/*---------------------------------------------------------------------------------------------------------------
+  const hasGet = !!prototype["__get"];
+
+  const hasGetAll = !!prototype["__getAll"];
+
+  const hasSet = !!prototype["__set"];
+
+  const hasUnset = !!prototype["__unset"];
+
+  const hasIsset = !!prototype["__isset"];
+
+  const hasIterator = !!prototype["__iterator"];
+
+    /*---------------------------------------------------------------------------------------------------------------
+	| classset
+	|----------------------------------------------------------------------------------------------------------------
+	| to determine if class has the property to magic.
+	| same with staticclassset
+	|
+	*/
+
+	const classset = hasGet || hasGetAll || hasSet || hasUnset || hasIsset || hasIterator
+    const staticclassset = "__setStatic" in $class || "__getStatic" in $class || "__callStatic" in $class;
+
+  /*---------------------------------------------------------------------------------------------------------------
 	| issetEnabled
 	|----------------------------------------------------------------------------------------------------------------
 	| A toggle switch for the __isset method
@@ -20,19 +45,9 @@ const __illusion__ = <I>($class: any, consider = false): I => {
 	|
 	*/
 
-	let issetEnabled = true
+  let issetEnabled = true;
 
-	/*---------------------------------------------------------------------------------------------------------------
-	| classset
-	|----------------------------------------------------------------------------------------------------------------
-	| to determine if class has the property to magic.
-	| same with staticclassset
-	|
-	*/
-
-	let classset = false, staticclassset = false;
-
-	/*---------------------------------------------------------------------------------------------------------------
+  /*---------------------------------------------------------------------------------------------------------------
 	| __iterator()
 	|----------------------------------------------------------------------------------------------------------------
 	| Caches "for(let row of class)"
@@ -42,37 +57,32 @@ const __illusion__ = <I>($class: any, consider = false): I => {
 	| class.properties = { iterator, Symbol.iterator }
 	*/
 
-	const iterator = $class.prototype['__iterator']
-	
-	if (!!iterator) 
-	{
-		classset = true
+  if (hasIterator) {
 
-		$class.prototype[Symbol.iterator] = function () {
+    $class.prototype[Symbol.iterator] = function () {
+      let data = this.__iterator(),
+        index = -1,
+        max = data.length;
 
-			let data = this.__iterator(), index = -1, max = data.length
+      return Array.isArray(data)
+        ? {
+            next() {
+              index++;
 
-	        return Array.isArray(data) ? 
-	        { 
+              return {
+                done: max === index,
 
-	            next() { 
+                value: data[index],
+              };
+            },
+          }
+        : (function* () {
+            for (let key in data) yield { key: key, value: data[key] };
+          })();
+    };
+  }
 
-	            	index++
-
-	                return {
-
-	                    done: max === index,
-
-	                    value: data[index]
-	                }
-	            }
-
-	        } : (function* () { for(let key in data) yield {key:key, value: data[key]} })()
-	    }
-	}
-
-
-	/*---------------------------------------------------------------------------------------------------------------
+  /*---------------------------------------------------------------------------------------------------------------
 	| classHandler
 	|----------------------------------------------------------------------------------------------------------------
 	| Trap for class instantiation for Proxy static class handler.
@@ -80,12 +90,10 @@ const __illusion__ = <I>($class: any, consider = false): I => {
 	|
 	*/
 
-	const classHandler: IllussionStaticProps = Object.create(null)
+  const classHandler: IllussionStaticProps = Object.create(null);
 
-	classHandler.construct = (target, args) => {
-
-
-		/*---------------------------------------------------------------------------------------------------------------
+  classHandler.construct = (target, args) => {
+    /*---------------------------------------------------------------------------------------------------------------
 		| instance
 		|----------------------------------------------------------------------------------------------------------------
 		| Wrapped class instance as a Proxy target
@@ -93,9 +101,9 @@ const __illusion__ = <I>($class: any, consider = false): I => {
 		|
 		*/
 
-		const instance = new $class(...args)
+    const instance = new $class(...args);
 
-		/*---------------------------------------------------------------------------------------------------------------
+    /*---------------------------------------------------------------------------------------------------------------
 		| instanceHandler
 		|----------------------------------------------------------------------------------------------------------------
 		| Trap for instance as Proxy handler.
@@ -103,147 +111,109 @@ const __illusion__ = <I>($class: any, consider = false): I => {
 		|
 		*/
 
-		const instanceHandler: IllussionInstanceProps = Object.create(null)
+    const instanceHandler: IllussionInstanceProps = Object.create(null);
 
-		/*---------------------------------------------------------------------------------------------------------------
+    /*---------------------------------------------------------------------------------------------------------------
 		| __get()
 		|----------------------------------------------------------------------------------------------------------------
 		| Catches "instance.property"
 		| We must set the class __get() property to set this method
 		|
-		*/ 
-
-		const get = $class.prototype['__get']
-		if (!!get) 
-		{
-			classset = true
-
-			instanceHandler.get = (target, name, receiver) => {
-
-				/*
+		*/
+    if (hasGet) {
+      instanceHandler.get = (target, name, receiver) => {
+        /*
 				| We need to turn off the __isset() trap for the moment to establish compatibility with PHP behaviour
 				| PHP's __get() method doesn't care about its own __isset() method, so neither should we
-				*/ 
+				*/
 
-				issetEnabled = false
-				const exists = name in target
-				issetEnabled = true
+        issetEnabled = false;
+        const exists = name in target;
+        issetEnabled = true;
 
-				if (('__illusion__' === name)) 
-				{
-					return target
-				}
+        if ("__illusion__" === name) {
+          return target;
+        }
 
-				if ('constructor' === name) 
-				{
-					return __illusion__(target[name])
-				}
+        if ("constructor" === name) {
+          return __illusion__(target[name]);
+        }
 
-				return exists ? target[name] : target.__get.call(receiver, name)
+        return exists ? target[name] : target.__get.call(receiver, name);
+      };
+    }
 
-			}
-		}
-
-		/*---------------------------------------------------------------------------------------------------------------
+    /*---------------------------------------------------------------------------------------------------------------
 		| __getAll()
 		|----------------------------------------------------------------------------------------------------------------
 		| Catches "instance.property"
 		| We must set the class __getAll() property to set this method
 		|
-		*/ 
+		*/
+    if (hasGetAll) {
+      instanceHandler.get = (target, name) => {
+        return target.__getAll.call(target, name);
+      };
+    }
 
-		const getAll = $class.prototype['__getAll']
-		if (!!getAll) 
-		{
-			classset = true
-
-			instanceHandler.get = (target, name) => {
-
-				return target.__getAll.call(target, name)
-
-			}
-		}
-
-		/*---------------------------------------------------------------------------------------------------------------
+    /*---------------------------------------------------------------------------------------------------------------
 		| __set()
 		|----------------------------------------------------------------------------------------------------------------
 		| Catches "instance.property = ..."
 		| We must set the class __set() property to set this method
 		|
-		*/ 
+		*/
+    if (hasSet) {
+      instanceHandler.set = (target, name, value) => {
+        if (name in target) {
+          target[name] = value;
+        } else {
+          target.__set.call(target, name, value);
+        }
 
-		const set = $class.prototype['__set']
-		if (!!set) 
-		{
-			classset = true
+        return true;
+      };
+    }
 
-			instanceHandler.set = (target, name, value) => {
-
-				if (name in target) {
-
-					target[name] = value
-
-				} else {
-
-					target.__set.call(target, name, value)
-
-				}
-
-				return true
-			}
-		}
-
-		/*---------------------------------------------------------------------------------------------------------------
+    /*---------------------------------------------------------------------------------------------------------------
 		| __isset()
 		|----------------------------------------------------------------------------------------------------------------
 		| Catches "'property' in instance"
 		| We must set the class __isset() property to set this method
 		|
-		*/ 
+		*/
+    if (hasIsset) {
+      instanceHandler.has = (target, name) => {
+        if (!issetEnabled) return name in target;
 
-		const isset = $class.prototype['__isset']
-		if (!!isset) 
-		{
-			classset = true
+        return target.__isset.call(target, name);
+      };
+    }
 
-			instanceHandler.has = (target, name) => {
-
-				if (!issetEnabled) return name in target
-
-				return target.__isset.call(target, name)
-			}
-		}
-
-		/*---------------------------------------------------------------------------------------------------------------
+    /*---------------------------------------------------------------------------------------------------------------
 		| __unset()
 		|----------------------------------------------------------------------------------------------------------------
 		| Catches "delete instance.property"
 		| We must set the class __unset() property to set this method
 		|
-		*/ 
+		*/
+    if (hasUnset) {
+      instanceHandler.deleteProperty = (target, name) => {
+        target.__unset.call(target, name);
 
-		const unset = $class.prototype['__unset']
-		if (!!unset) 
-		{
-			classset = true
+        return true;
+      };
+    }
 
-			instanceHandler.deleteProperty = (target, name) => {
-
-				target.__unset.call(target, name)
-
-				return true
-			}
-		}
-
-		/*
+    /*
 		| if one of the properties above was established we return the Proxy else the true instance
 		| @return instance
 		*/
 
-		return classset ? new Proxy(instance, instanceHandler) : instance
-	}
+    return classset ? new Proxy(instance, instanceHandler) : instance;
+  };
 
-	/*---------------------------------------------------------------------------------------------------------------
+  /*---------------------------------------------------------------------------------------------------------------
 	| __getStatic()
 	|----------------------------------------------------------------------------------------------------------------
 	| Catches "class.property"
@@ -251,18 +221,17 @@ const __illusion__ = <I>($class: any, consider = false): I => {
 	|
 	*/
 
-	if ('__getStatic' in $class) {
-		staticclassset = true
-		classHandler.get = (target, name) => {
-			if (name in target) {
-				return target[name]
-			} else {
-				return target.__getStatic(name)
-			}
-		}
-	}
+  if ("__getStatic" in $class) {
+    classHandler.get = (target, name) => {
+      if (name in target) {
+        return target[name];
+      } else {
+        return target.__getStatic(name);
+      }
+    };
+  }
 
-	/*---------------------------------------------------------------------------------------------------------------
+  /*---------------------------------------------------------------------------------------------------------------
 	| __setStatic()
 	|----------------------------------------------------------------------------------------------------------------
 	| Catches "class.property = ..."
@@ -270,24 +239,19 @@ const __illusion__ = <I>($class: any, consider = false): I => {
 	|
 	*/
 
-	if ('__setStatic' in $class)  {
-		staticclassset = true
-		classHandler.set = (target, name, value) => {
-            if (name in target) {
+  if ("__setStatic" in $class) {
+    classHandler.set = (target, name, value) => {
+      if (name in target) {
+        target[name] = value;
+      } else {
+        target.__setStatic(name, value);
+      }
 
-                target[name] = value
+      return true;
+    };
+  }
 
-            } else {
-
-                target.__setStatic(name, value)
-
-            }
-
-            return true
-		}
-	}
-
-	/*---------------------------------------------------------------------------------------------------------------
+  /*---------------------------------------------------------------------------------------------------------------
 	| __callStatic()
 	|----------------------------------------------------------------------------------------------------------------
 	| Catches "class.property()"
@@ -299,32 +263,27 @@ const __illusion__ = <I>($class: any, consider = false): I => {
 	| this method will contradict the __getStatic() because they have been set in same method get()
 	|
 	| only __callStatic will be invoke if they are both declared 
-	*/ 
+	*/
 
-	if ('__callStatic' in $class) 
-	{	
-		staticclassset = true
+  if ("__callStatic" in $class) {
+    classHandler.get = (target, name, receiver) => {
+      if ("__illusion__" === name) {
+        return target;
+      }
 
-		classHandler.get = (target, name, receiver) => {
+      return target[name] || target.__callStatic.call(receiver, name);
+    };
+  }
 
-			if (('__illusion__' === name)) 
-			{
-				return target
-			}
-
-			return target[name] || target.__callStatic.call(receiver, name)
-
-		}
-	}
-
-	/*---------------------------------------------------------------------------------------------------------------
+  /*---------------------------------------------------------------------------------------------------------------
 	| return
 	|----------------------------------------------------------------------------------------------------------------
 	| if none of the method above was declared. it will return the class itself 
 	*/
-	
-	return staticclassset || classset || consider? new Proxy($class, classHandler) : $class
 
-}
+  return staticclassset || classset
+    ? new Proxy($class, classHandler)
+    : $class;
+};
 
-export default __illusion__
+export default __illusion__;
